934ddc796dee721f8222ab97ae95aea7fbaaa6fc
Raymond <musaray001@gmail.com>
2024-06-19 23:17:43 +0200

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+Sum = number1 + number2
+Difference = number1 - number2
+Product = number1*number2
+
+print("Addition of 10 and 5 is", Sum)
+print("Subtraction og 10 and 5 is", Difference)
+print("Multiplication of 10 and 5 is", Product)
