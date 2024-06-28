68220bb78ab3a856475a1c807554b95e91f5a2c5
Raymond <musaray001@gmail.com>
2024-06-28 13:25:16 +0200

    Multiplication Table Generator

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,7 +3,7 @@ number =int(input('Enter a number to see its multiplication table: '))
 i = 1
 
 for i in range(1,11):
-    product = number * i
-    print(number, '*', i, '=', product)
+   
+    print(f"{number} * {i} = {mumber * i}")
 
     i -= 1
