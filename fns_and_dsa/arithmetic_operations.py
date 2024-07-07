af72f009bdf3c8816df568d6d9bde3e67a1731db
Raymond <musaray001@gmail.com>
2024-07-07 04:04:09 -0700

    Arithmetic Operations Function - inserted colon in zero dvision condition

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,7 +2,7 @@ def perform_operation(num1, num2, operation):
     if operation == 'add': result = num1 + num2
     if operation =='subtract': result = num1 - num2
     if operation =='multiply': result = num1 * num2
-    if operation =='divide'
+    if operation =='divide':
         if num2 == 0.0: 
             result = 'Not divisible by 0'
         else: : result = num1 / num2
