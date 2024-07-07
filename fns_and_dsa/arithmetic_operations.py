b4dffa9e31af6150e045ec1db7ce7050283ef4c9
Raymond <musaray001@gmail.com>
2024-07-07 04:18:44 -0700

    TAsk 0 - replaced 0.0 with 0 in zero division condition

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,8 +3,8 @@ def perform_operation(num1, num2, operation):
     elif operation =='subtract': result = num1 - num2
     elif operation =='multiply': result = num1 * num2
     elif operation =='divide':
-        if num2 == 0.0: 
+        if num2 == 0: 
             result = 'Not divisible by 0'
-        else: : result = num1 / num2
+        else: result = num1 / num2
             
     return result
