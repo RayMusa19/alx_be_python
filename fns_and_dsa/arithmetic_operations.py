6540740d2eb313d5a1c9f8b71c845e1e0e80ca59
Raymond <musaray001@gmail.com>
2024-07-07 03:15:55 +0200

    Arithmetic Operations Function - Division by 0 scenario correction

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,7 +3,9 @@ def perform_operation(num1, num2, operation):
     if operation =='subtract': result = num1 - num2
     if operation =='multiply': result = num1 * num2
     if operation =='divide'
-        if num2 == 0.0: result = 'Not divisible by 0'
+        if num2 == 0.0: 
+            result = num1/num2
+            print('Not divisible by 0')
         else: : result = num1 / num2
             
     return result
