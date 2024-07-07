d45d10c626775081d26195c6b8a7f0ac9f8b95aa
Raymond <musaray001@gmail.com>
2024-07-07 02:49:56 +0200

    Arithmetic Operations Function correction

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,10 +1,9 @@
 def perform_operation(num1, num2, operation):
-    if num2!=0:
-        match operation:
-            case 'add': result = num1 + num2
-            case 'subtract': result = num1 - num2
-            case 'multiply': result = num1 * num2
-            case 'divide': result = num1 / num2
-        return result
-    else:
-        return 'Not divisible by 0'
+    if operation == 'add': result = num1 + num2
+    if operation =='subtract': result = num1 - num2
+    if operation =='multiply': result = num1 * num2
+    if operation =='divide'
+        if num2 = 0.0: result = 'Not divisible by 0'
+        else: : result = num1 / num2
+            
+    return result
