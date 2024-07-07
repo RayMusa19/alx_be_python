9fd200e8e7d303b6e4a1d5ddb0106b3ba829d6a2
Raymond <musaray001@gmail.com>
2024-07-07 04:06:38 -0700

    Arithmetic Operations Function - replaced if with elif

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,8 @@
 def perform_operation(num1, num2, operation):
     if operation == 'add': result = num1 + num2
-    if operation =='subtract': result = num1 - num2
-    if operation =='multiply': result = num1 * num2
-    if operation =='divide':
+    elif operation =='subtract': result = num1 - num2
+    elif operation =='multiply': result = num1 * num2
+    elif operation =='divide':
         if num2 == 0.0: 
             result = 'Not divisible by 0'
         else: : result = num1 / num2
