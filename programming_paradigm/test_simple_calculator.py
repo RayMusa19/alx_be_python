580e7c9d5ca4752baf640001c170675d9ad6db4e
Raymond Musa <musaray001@gmail.com>
2024-07-14 13:10:07 +0000

    Task 1

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,31 +1,31 @@
-import unittest
+mport unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
-    def test_add(self):
-        self.assertEqual(self.calculator.add(1, 2), 3)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
-        self.assertEqual(self.calculator.add(-1, -1), -2)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
 
-    def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(10, 5), 5)
-        self.assertEqual(self.calculator.subtract(-1, 1), -2)
-        self.assertEqual(self.calculator.subtract(-1, -1), 0)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
 
-    def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(3, 7), 21)
-        self.assertEqual(self.calculator.multiply(-1, 1), -1)
-        self.assertEqual(self.calculator.multiply(-1, -1), 1)
-        self.assertEqual(self.calculator.multiply(0, 1), 0)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(3, 7), 21)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(0, 1), 0)
 
-    def test_divide(self):
-        self.assertEqual(self.calculator.divide(8, 2), 4)
-        self.assertEqual(self.calculator.divide(-6, 2), -3)
-        self.assertEqual(self.calculator.divide(-6, -2), 3)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(8, 2), 4)
+        self.assertEqual(self.calc.divide(-6, 2), -3)
+        self.assertEqual(self.calc.divide(-6, -2), 3)
         
         with self.assertRaises(ValueError):
-            self.calculator.divide(1, 0)
+            self.calc.divide(1, 0)
