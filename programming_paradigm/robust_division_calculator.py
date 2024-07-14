f0314977ad271be7a845bbe0e43795da42e84715
Raymond Musa <musaray001@gmail.com>
2024-07-14 12:32:46 +0000

    Task 1

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,18 +1,23 @@
 def safe_divide(numerator, denominator):
-    
+
     try:
         numerator1 = float(numerator)
         denominator1 = float(denominator)
 
         division = numerator1 / denominator1
+        return division
 
     except ZeroDivisionError:
-        division = print('Error: Cannot divide by zero.')
+        return('Error: Cannot divide by zero.')
+        
 
     except ValueError:
-        if isinstance(float(numerator, denominator), (float)):
+        if isinstance(float(numerator), (float)) and isinstance(float(denominator), (float)):
             division = (float(numerator) / float(denominator))
-        else:
-            division = print('Error: Please enter numeric values only.')
+            return ('The result of the division is',division)
 
-    return division
+        else:
+            print('Error: Please enter numeric values only.')
+            return
+    
+
